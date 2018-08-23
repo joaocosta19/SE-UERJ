22bb0671c98bb87e408729f1e19870a51d489430
joaocosta19 <42645018+joaocosta19@users.noreply.github.com>
2018-08-23 13:09:47 -0300

    Blink-Switch-01-joaocosta
    Fazer o LED acompanhar o estado do botão

---
+++ Bilnk-Switch-01-joaocosta.ino
@@ -0,0 +1,14 @@
+#define LED_PIN 13
+#define BUT_PIN  7
+ //Fazer o LED acompanhar o estado do botão
+ 
+void setup () {
+    pinMode(13, OUTPUT);       // Enable pin 13 for digital output
+    pinMode(7, INPUT);        // Enable pin  2 for digital input
+}
+ 
+void loop () {
+    int but = digitalRead(7); // Read button state
+    digitalWrite(13, but);     // Copy state to LED
+}
+
