04d17d24801b121621a725e28cfb0ee079b6c74f
joaocosta19 <42645018+joaocosta19@users.noreply.github.com>
2018-08-25 11:35:05 -0300

    IO-Basico-Slide-3-01-joaocosta
    Tarefa 01 João Costa

---
+++ IO-Basico-Slide-3-01-joaocosta.ino
@@ -0,0 +1,13 @@
+#define LED_PIN 9
+#define BUT_PIN 7
+ //I/O Básico - SLIDE 3
+void setup () {
+    pinMode(7, INPUT);        // Enable pin  7 for digital input
+    pinMode(9, OUTPUT);       // Enable pin 9 for digital output
+}
+void loop () {
+  int val = digitalRead (7);
+  digitalWrite (9, HIGH);
+}
+
+
