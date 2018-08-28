8df33f24520dce49b415af36df435255a0a39e92
joaocosta19 <42645018+joaocosta19@users.noreply.github.com>
2018-08-28 15:24:12 -0300

    Blink-Switch-01-joaocosta-v2

---
+++ Blink-Switch-01-joaocosta-v2.ino
@@ -0,0 +1,14 @@
+#define LED_PIN 13
+#define BUT_PIN  2
+ //Fazer o LED acompanhar o estado do botão slide 8
+ 
+void setup () {
+    pinMode(13, OUTPUT);       // Enable pin 13 for digital output
+    pinMode(2, INPUT);        // Enable pin 2 for digital input
+}
+ 
+void loop () {
+    int but = digitalRead(2); // Read button state
+    digitalWrite(13, but);     // Copy state to LED
+}
+
