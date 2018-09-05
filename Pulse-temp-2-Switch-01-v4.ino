73cf05a0f3b604a9b68291ade5b4b3ae278e05fd
joaocosta19 <42645018+joaocosta19@users.noreply.github.com>
2018-09-05 13:56:04 -0300

    Tarefa - 01 - Pisca-Bot1-Bot2
    Pisca-Bot1-Bot2

---
+++ Pulse-temp-2-Switch-01-v4.ino
@@ -0,0 +1,41 @@
+
+#define pinLedVm 13
+#define pinBotao1 2
+#define pinBotao 7
+
+boolean estadoBotao = true;
+boolean estAntBotao = true;
+boolean estadoPisca = false;
+
+unsigned long lastTime;
+unsigned long speeding = 500;         //Velocidade do Pisca
+
+void setup() {
+
+  lastTime = millis();
+  
+  pinMode(pinLedVm, OUTPUT);
+  pinMode(pinBotao, INPUT_PULLUP);
+  pinMode(pinBotao1, INPUT_PULLUP);
+}
+
+void loop() {
+
+  if(    !(digitalRead(pinBotao))   ){
+    // Aumenta a frequncia do Pisca
+    speeding = speeding - 1;
+  }
+  if(    !(digitalRead(pinBotao1))   ){
+    // Aumenta a frequncia do Pisca
+    speeding = speeding + 1;
+  }
+  
+  if ( (millis() - lastTime) >= speeding) {
+      estadoPisca = !estadoPisca;
+      digitalWrite( pinLedVm, estadoPisca);
+      lastTime=millis();
+  }
+ 
+
+  delay(10);
+}
