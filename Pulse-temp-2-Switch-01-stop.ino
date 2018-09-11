0b537e2682f7d269becd72b87b3bdbebf6d0d337
joaocosta19 <42645018+joaocosta19@users.noreply.github.com>
2018-09-11 10:02:02 -0300

    Tarefa-00-Botao1+2-Parar
    Tarefa-00-Botao1+2-Parar-Ultima

---
+++ Pulse-temp-2-Switch-01-stop.ino
@@ -0,0 +1,45 @@
+
+#define pinLedVm 13
+#define pinBotao1 2
+#define pinBotao 7
+
+boolean estadoBotao = true;
+boolean estAntBotao = true;
+boolean estadoPisca = false;
+
+unsigned long lastTime;
+unsigned long speeding = 500;         //Velocidade do Pisca
+
+void setup() {
+
+  lastTime = millis();
+  
+  pinMode(pinLedVm, OUTPUT);
+  pinMode(pinBotao, INPUT_PULLUP);
+  pinMode(pinBotao1, INPUT_PULLUP);
+}
+
+void loop() {
+
+  if(    !(digitalRead(pinBotao))   ){
+    // Diminui a frequencia do Pisca
+    speeding = speeding - 1;
+  }
+  if(    !(digitalRead(pinBotao1))   ){
+    // Aumenta a frequencia do Pisca
+    speeding = speeding + 1;
+  }
+  if(   !(digitalRead(pinBotao)) && !(digitalRead(pinBotao1))   ){
+    // Parar pisca em menos de 500ms Pressionando botao1&2
+    speeding;
+    digitalWrite( pinLedVm, HIGH);
+  }
+  if ( (millis() - lastTime) >= speeding) {
+      estadoPisca = !estadoPisca;
+      digitalWrite( pinLedVm, estadoPisca);
+      lastTime=millis();
+  }
+ 
+
+  delay(10);
+}
