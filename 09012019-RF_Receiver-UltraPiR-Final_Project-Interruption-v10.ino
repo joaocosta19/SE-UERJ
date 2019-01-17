475cb232e7ef08eae4d20d98d5da5fb1e969767c
joaocosta19 <42645018+joaocosta19@users.noreply.github.com>
2019-01-17 16:37:47 -0200

    17012019-Final Project-UltraPirCar-Interruption
    Erro no "15012019-RF_Receiver-UltraPiR-Final_Project-Interruption-v12.ino" - "Erro compilando para a placa Arduino/Genuino Uno"

---
+++ 09012019-RF_Receiver-UltraPiR-Final_Project-Interruption-v10.ino
@@ -0,0 +1,115 @@
+#include <TimerOne.h> //TIMER FUNCTION FOR PIR SENSOR
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+//Armazena os dados recebidos
+int recebidos [1];
+
+//Inicializa a placa nos pinos 9 (CE) e 10 (CS) do Arduino
+RF24 radio(9,10);
+
+//Define o endereco para comunicacao entre os modulos
+const uint64_t pipe = 0xE14BC8F482LL;
+
+//Define os pinos dos leds = Rodas
+int LED1 = 5;  //Continua Andando
+
+//Valores para PiR
+int pinPiR = 6;        // value read from the port 6
+
+void setup() {
+
+//Define os pinos dos leds/Rodas como Saída
+  pinMode(LED1, OUTPUT);
+  pinMode(pinPiR, INPUT);
+  
+//Inicializa a serial
+  Serial.begin(57600);
+
+//Inicializa a comunicacao
+  radio.begin();
+  
+//Entra em modo de recepcao
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+
+  // initialize timer1 
+
+  noInterrupts();           // disable all interrupts
+
+  TCCR1A = 0;
+
+  TCCR1B = 0;
+
+  TCNT1  = 0;
+
+  OCR1A = 31250;            // compare match register 16MHz/256/2Hz 31250 / 62500 / 15625
+
+  TCCR1B |= (1 << WGM12);   // CTC mode
+
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
+
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+
+  interrupts();             // enable all interrupts
+
+}
+
+ISR(TIMER1_COMPA_vect)         // rotina de serviço de interrupção que envolve uma função definida pelo usuário fornecida pelo attachInterrupt
+{
+//#if 0
+if (digitalRead(pinPiR) == HIGH)
+    {
+    digitalWrite(LED1, LOW);
+    }
+      else 
+      {
+      recebidos[0];
+      }
+//#endif     
+}
+
+void loop() {
+
+  //Verifica se ha sinal de radio
+  if (radio.available())
+  {
+    
+    bool done = false;    
+    while (!done)
+    {
+      radio.read(recebidos,1);                  //Havia erro de sintaxe retirado "done = radio.read(recebidos,1)"
+      Serial.print("Dados recebidos : ");    
+      Serial.println(recebidos[0]);
+      
+      //Se recebeu o numero 1, acende o LED1/IN do Relé para acionamento das Rodas
+  if (recebidos[0] == 1)
+      {
+        delay(10);
+        digitalWrite(LED1, HIGH);
+      }
+      else 
+      {
+        digitalWrite(LED1, LOW);
+       }
+
+  if (digitalRead(pinPiR) == HIGH)
+      {
+       digitalWrite(LED1, LOW);
+       }
+      else 
+      {
+      recebidos[0];
+      }
+       
+      //delay(100);
+      
+       }
+   
+  }
+  else
+  {
+  Serial.println("Aguardando dados...");
+  }
+}
---
+++ 15012019-RF_Receiver-UltraPiR-Final_Project-Interruption-v12.ino
@@ -0,0 +1,125 @@
+#include <TimerOne.h> //TIMER FUNCTION FOR PIR SENSOR
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+//Armazena os dados recebidos
+int recebidos [1];
+
+//Inicializa a placa nos pinos 9 (CE) e 10 (CS) do Arduino
+RF24 radio(9,10);
+
+//Define o endereco para comunicacao entre os modulos
+const uint64_t pipe = 0xE14BC8F482LL;
+
+//Define os pinos dos leds = Rodas
+int LED1 = 5;  //Continua Andando
+
+//Valores para PiR
+int pinPiR = 6;        // value read from the port 6
+
+void setup() {
+
+//Define os pinos dos leds/Rodas como Saída
+  pinMode(LED1, OUTPUT);
+  pinMode(pinPiR, INPUT);
+  
+//Inicializa a serial
+  Serial.begin(57600);
+
+//Inicializa a comunicacao
+  radio.begin();
+  
+//Entra em modo de recepcao
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+
+  // initialize timer1 
+
+  TIMSK1 = 3;                        // ativa o Timer/Counter1 com os modos dos bits 0 e 1, ou seja,// ativa Comparador A e detector de overflow
+
+  TCCR1A = 0;                        // desabilita inicialmente o timer
+  TCCR1B = 0;                        // desabilita inicialmente o timer
+ 
+ }
+ 
+void zeroCross() {
+TCCR1B=4;                            // habilita o timer no modo que divide o clock real por 256
+TCNT1=0;                                // zera o contador do timer
+ }
+ 
+ISR(TIMER1_COMPA_vect)         // rotina de serviço de interrupção que envolve uma função definida pelo usuário fornecida pelo attachInterrupt
+{
+//#if 0
+if (digitalRead(pinPiR) == HIGH)  //Se Alguém passar na frente do carro o mesmo para através da sensibilidade do sensor PiR
+    {
+    TCNT1 = 49911;               // inicia timer com valor para que estouro ocorra em 1 segundo//65536-(16MHz/1024/1Hz) = 49911 = 0xC2F7
+    digitalWrite(LED1, LOW);
+    }
+      else 
+      {
+      recebidos[0];
+      }
+//#endif     
+}
+
+ISR(TIMER1_OVF_vect) {
+    
+if (digitalRead(pinPiR) == HIGH)
+    {  
+    TCCR1B = 0;                // desativa o timer que foi inicialmente ativado pela nossa funcao zeroCross().
+    digitalWrite(LED1, LOW);
+    }
+      else 
+      {
+      recebidos[0];
+      }
+}
+
+void loop() {
+
+  //Verifica se ha sinal de radio
+  if (radio.available())
+  {
+    
+    bool done = false;    
+    while (!done)
+    {
+      radio.read(recebidos,1);                  //Havia erro de sintaxe retirado "done = radio.read(recebidos,1)"
+      Serial.print("Dados recebidos : ");    
+      Serial.println(recebidos[0]);
+      
+      //Se recebeu o numero 1, acende o LED1/IN do Relé para acionamento das Rodas
+  if (recebidos[0] == 1)
+      {
+        delay(10);
+        digitalWrite(LED1, HIGH);
+      }
+      else 
+      {
+        digitalWrite(LED1, LOW);
+       }
+
+  if (digitalRead(pinPiR) == HIGH) //-->> OK//Se Alguém passar na frente do carro o mesmo para através da sensibilidade do sensor PiR
+    {
+      attachInterrupt(0,zeroCross, RISING); 
+      digitalWrite(LED1, LOW);
+       }
+      else 
+      {
+      recebidos[0];
+      detachInterrupt(0); 
+      }
+       
+      //delay(100);
+      
+       }
+   
+  }
+  else
+  {
+  Serial.println("Aguardando dados...");
+  }
+}
